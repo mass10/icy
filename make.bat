e3427e117690f3abd02380858d9b8c5b98be5d3c
Irisawa Masaru <mass10.github@gmail.com>
2020-01-19 23:53:31 +0900

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,9 +1,11 @@
 @SETLOCAL
 @ECHO OFF
 
-DEL /Q dist\*
+DEL /S /Q dist\*
 
 CALL yarn tsc
+
+REM CALL yarn browserify --entry dist/src/icy.js --require requirejs
 
 IF EXIST ".setting.bat" (
 	CALL .setting.bat
