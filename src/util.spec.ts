e9b9c3c2c8a17f16a07302f185e666dc4a3e9905
Irisawa Masaru <mass10.github@gmail.com>
2020-02-03 23:09:26 +0900

    Update util.spec.ts

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,7 +1,13 @@
 import { util } from './util';
 
-describe("util", () => {
-    it("xxxxxx", () => {
-        expect(util.getTrue()).toBe(true);
-    });
+describe("util をテストします。", () => {
+
+	it("util.getTrue() が true を返すこと。", () => {
+		expect(util.getTrue()).toBe(true);
+	});
+
+	it("util.getTimestamp() がタイムスタンプ文字列を返すこと。", () => {
+		const timestamp = util.getTimestamp();
+		expect(timestamp).toMatch(/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]/);
+	});
 });
